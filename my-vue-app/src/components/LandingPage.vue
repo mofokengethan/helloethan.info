8d7f2e9b45dfb890e447b06154e93f800194c625
LDNHRH <ethanmofokeng@yahoo.com>
2025-10-08 23:04:18 -0400

    landing page v1

--- my-vue-app/src/components/LandingPage.vue
+++ my-vue-app/src/components/LandingPage.vue
@@ -1,19 +1,53 @@
 <script setup>
-// import { ref } from 'vue'
+import { marked } from 'marked'
 
 defineProps({
   msg: String,
 })
 
-// const count = ref(0)
+const markdown = `
+| Android | iOS | Web |
+|--------|--------|--------|
+| | | [helloethan.info]() |
+| | | [arabica.press]() |
+| [LanguageLifeOS]() |
+| [Learning Dutch]() |
+| | [AmourOS]() |
+| | [RoomServiceOS]() |
+| | [NorskDuck]() |
+`
+
+const markdownHtml = marked(markdown)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-<!--  <div class="card">-->
-<!--    <button type="button" @click="count++">count is {{ count }}</button>-->
-<!--  </div>-->
+  <p><i>Native Android iOS Engineer</i></p>
+  <h1>👋 {{ msg }} 💻</h1>
+  <div class="button-row">
+    <a href="https://www.linkedin.com/in/mofokengethan" class="btn">About</a>
+    <a href="" class="btn">Android</a>
+    <a href="" class="btn">iOS</a>
+    <a href="" class="btn">Web</a>
+    <a class="btn">🔭</a>
+    <a href="" class="btn">About</a>
+    <a href="" class="btn">About</a>
+    <a href="" class="btn">About</a>
+    <a href="" class="btn">About</a>
+  </div>
+  <p><b>Mobile Software Engineer II</b> @ Excellus BCBS</p>
+  <p><b>Mobile Software Engineer I</b> @ Excellus BCBS</p>
+  <p><b>Religious Affair Specialist (56M)</b> @ Army</p>
+  <br>
+  <h1>Published ✅</h1>
+  <div class="center" v-html="markdownHtml"></div>
+  <br>
+  <p><b>Software Development, M.Sc.</b> @ University of Strathclyde</p>
+  <p><b>IT Data Management, B.Sc. </b> @ Colorado Technical University</p>
+  <br>
+  <h1>Languages, Frameworks and Tools ⚒️</h1>
+  <p><b style="color: #F87666">Kotlin</b>, Java, <b style="color: #58B09C">Swift</b>, Html, CSS, Javascript, Typescript</p>
+  <p>Vue, <b style="color: #58B09C">SwiftUI</b>, Kotlin Multiplatform, <b style="color: #F87666">Jetpack Compose</b>, XML</p>
+  <p>Git, Azure DevOps, Firebase Analytics, Adobe XD, Figma, Rive, Postman, Android Studio, Xcode, Jetbrains IDEs</p>
 </template>
 
 <style scoped>
